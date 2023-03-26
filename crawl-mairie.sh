b831728b4ec9bc6a94427d3fb05eeb66213d18ce
Antoine Barthelemy <antoine@linux.com>
2023-03-26 20:06:28 +0200

    feat(async): ok

--- crawl-mairie.sh
+++ crawl-mairie.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+echo '[]' | jq '[range(0;42)| now + . * 86400 | strftime("%Y-%m-%d")]' | grep -oE '[0-9]{4}-0(3|4)-[0-9]{2}' > date
+curl -L -c rennes.txt 'https://eappointment2.rennes.fr/eAppointment/appointment.do?preselectservice=RDV' 2> /dev/null > /dev/null
 function dispo() {
 	slots="$(echo $2)"
 	if $3; then
@@ -8,10 +9,6 @@ function dispo() {
 		printf "%-37s\e[90m%s\e[0m\n" "$1" "$2"
 	fi
 }
-
-echo '[]' | jq '[range(0;42)| now + . * 86400 | strftime("%Y-%m-%d")]' | grep -oE '[0-9]{4}-0(3|4)-[0-9]{2}' > date
-curl -L -c rennes.txt 'https://eappointment2.rennes.fr/eAppointment/appointment.do?preselectservice=RDV' 2> /dev/null > /dev/null
-
 function async_curl() {
 	endpoint='https://eappointment2.rennes.fr/eAppointment/dwr/call/plaincall/AjaxSelectionFormFeeder.getClosedDaysList.dwr'
 	data="callCount=1\nnextReverseAjaxIndex=0\nc0-scriptName=AjaxSelectionFormFeeder\nc0-methodName=getClosedDaysList\nc0-id=0\nc0-param0=string:$2\nc0-param1=string:20012\nbatchId=5\ninstanceId=0\npage=%%2FeAppointment%%2Felement%%2Fjsp%%2Fappointment.jsp\nscriptSessionId=\n"
@@ -23,7 +20,6 @@ function async_curl() {
 		dispo "$1: " "Pas de RDV" false
 	fi
 }
-
 async_curl "Mairie Centre - Victor Hugo" "site7" &
 async_curl "Mairie de quartier Blosne" "site3" &
 async_curl "Mairie de quartier Brequigny" "site8" &
